8ba43c4c118508641bb651f84b017f075ea9a21f
takeAjoy <takeAjoy1@gmail.com>
2026-05-22 22:15:36 +0400

    abbreviate

---
+++ 6 kyu/abbreviate.js
@@ -0,0 +1,14 @@
+/*
+Link: https://www.codewars.com/kata/5375f921003bf62192000746
+Author: wthit56
+Tags: Strings, Fundamentals
+*/
+function abbreviate(string) {
+    return string.replaceAll(/\p{L}+/gu, (word) => {
+        let n = word.length;
+        if (n >= 4) {
+            return word[0] + (n - 2) + word[n - 1];
+        }
+        return word;
+    });
+}
