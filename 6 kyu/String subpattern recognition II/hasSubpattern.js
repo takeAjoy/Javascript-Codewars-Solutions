772102d97b1deaf48ffbf65988acb9f57634b893
takeAjoy <takeAjoy1@gmail.com>
2026-05-25 04:00:25 +0400

    hasSubpattern

---
+++ 6 kyu/String subpattern recognition II/hasSubpattern.js
@@ -0,0 +1,17 @@
+/*
+Link: https://www.codewars.com/kata/5a4a391ad8e145cdee0000c4
+Kata: String subpattern recognition II
+Author: GiacomoSorbi
+Tags: Strings, Regular Expressions, Fundamentals
+*/
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b);
+}
+
+function hasSubpattern(string) {
+    let obj = {};
+    for (let k of string) {
+        obj[k] = (obj[k] || 0) + 1;
+    }
+    return Object.values(obj).reduce((acc, el) => gcd(acc, el)) > 1
+}
