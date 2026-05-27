89107399341ed2e3e4eb9947f67e2327dc2e3592
takeAjoy <takeAjoy1@gmail.com>
2026-05-27 22:07:53 +0400

    updated guessColour code.

--- 6 kyu/Hat Game/guessColour.js
+++ 6 kyu/Hat Game/guessColour.js
@@ -4,11 +4,13 @@ Kata: Hat Game
 Author: Kacarott
 Tags: Riddles
 */
+const countRedAmount = (arr) => arr.reduce((a, c) => a + (c == "Red"), 0)
+
 const guessColour = (g, h) => {
-    const r = h.reduce((a, c) => a + (c == "Red"), 0);
+    const r = countRedAmount(h)
     return !g.length
         ? r % 2 ? "Red" : "Blue"
-        : ((g.slice(1).reduce((a, c) => a + (c == "Red"), 0) + r) % 2 ==
+        : ((countRedAmount(g.slice(1)) + r) % 2 ==
             (g[0] == "Red"))
             ? "Blue"
             : "Red";
