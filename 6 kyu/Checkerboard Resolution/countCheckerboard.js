aebe4c5474d870ecb752f5d3763618758a122f15
takeAjoy <takeAjoy1@gmail.com>
2026-05-27 20:39:33 +0400

    updated countCheckerboard solution

--- 6 kyu/Checkerboard Resolution/countCheckerboard.js
+++ 6 kyu/Checkerboard Resolution/countCheckerboard.js
@@ -8,24 +8,10 @@ function countCheckerboard(width, height, resolution) {
   const w = 1n * width, h = 1n * height, r = 1n * resolution;
   const W = w / r, H = h / r;
   const rh = h % r, rw = w % r;
-  const bs = (W * H) / 2n;
-  let b = bs * r * r;
-
-  if (rh > 0n) {
-    const bss = H % 2n === 0n ? W / 2n : (W + 1n) / 2n;
-    b += bss * rh * r;
-  }
-
-  if (rw > 0n) {
-    const bss = W % 2n === 0n ? H / 2n : (H + 1n) / 2n;
-    b += bss * rw * r;
-  }
-
-  if (rw > 0n && rh > 0n && (W + H) % 2n === 1n) {
-    b += rh * rw;
-  }
-
-  return b;
+  let b = (W * H) / 2n * r ** 2n;
+  (rh > 0n) && (b += (H % 2n === 0n ? W / 2n : (W + 1n) / 2n) * rh * r);
+  (rw > 0n) && (b += (W % 2n === 0n ? H / 2n : (H + 1n) / 2n) * rw * r);
+  return (W + H) % 2n === 1n ? b + rh * rw : b;
 }
 
 module.exports = countCheckerboard;
